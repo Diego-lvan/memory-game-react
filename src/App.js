33e45953dd0c51ac06520c02b303773f4f446066
Diego <dcorreanavarrete@gmail.com>
2022-06-29 13:11:52 -0500

    moving states from Cards.js to App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { useState, createContext } from "react";
 import Title from "./components/Title/Title.css";
-import Cards from "./components/Cards/Cards";
+import Cards from "./components/Game/Cards";
 //styles
 import GlobalStyles from "./GlobalStyles";
+import InfoGame from "./components/Game/InfoGame";
 export const AppContext = createContext();
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [moves, setMoves] = useState(0);
+  const [seconds, setSeconds] = useState(1);
 
   return (
     <AppContext.Provider
       value={{
         loading,
         setLoading,
+        moves,
+        setMoves,
+        seconds,
+        setSeconds,
       }}
     >
       <div>
         <GlobalStyles />
         <Title />
-
         <Cards />
+        <InfoGame />
       </div>
     </AppContext.Provider>
   );
---
+++ src/components/Game/Cards.js
@@ -0,0 +1,68 @@
+import React, { useState, useContext, useEffect } from "react";
+import Card from "./Card";
+import styled from "styled-components";
+import axios from "axios";
+import { AppContext } from "../../App";
+
+const URL = "https://rickandmortyapi.com/api/character/1,2,3,4,5,6,7,8,9,10";
+
+const Cards = ({ className }) => {
+  const [characters, setCharacters] = useState([]);
+  const [remaining, setRemaining] = useState([]);
+  const [previous, setPrevious] = useState(null);
+  const [waiting, setWaiting] = useState(false);
+
+  const { loading, setLoading } = useContext(AppContext);
+
+  const getCharacters = async () => {
+    const res = await axios.get(URL);
+    const chars = res.data;
+    const shuffle1 = chars.sort(() => 0.5 - Math.random());
+    const shuffle2 = chars.sort(() => 0.5 - Math.random());
+    setCharacters([...shuffle2, ...shuffle1]);
+    setRemaining([...shuffle2, ...shuffle1]);
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getCharacters();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  if (loading) {
+    return <h1 style={{ textAlign: "center", fontSize: "25px" }}>Loading...</h1>;
+  }
+  return (
+    <>
+      <div className={className}>
+        {characters.map((char, i) => {
+          return (
+            <Card
+              id={char.id}
+              index={i}
+              characters={characters}
+              remaining={remaining}
+              setRemaining={setRemaining}
+              previous={previous}
+              setPrevious={setPrevious}
+              waiting={waiting}
+              setWaiting={setWaiting}
+            />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+const CardsStyled = styled(Cards)`
+  display: grid;
+  grid-template-columns: repeat(4, 90px);
+  width: 100%;
+  margin: 0 10px 20px 10px;
+  justify-content: center;
+  gap: 3px;
+`;
+
+export default CardsStyled;
